643ef57e8b3a7ab7d7a27c398ac5c000f0eafa38
Fabien Butz <fabien@ruoka.app>
2022-03-14 17:54:53 +0100

    --- INITIAL COMMIT ---

---
+++ pages/api/mila-subscription.js
@@ -0,0 +1,25 @@
+import initStripe from "stripe";
+import { buffer } from "micro";
+
+export const config = { api: { bodyParser: false } };
+
+export default function handler(req, res) {
+  const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
+  const signature = req.headers("stripe-signature");
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+  const reqBuffer = await buffer(req);
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(`Webhook error: ${error.message}`);
+  }
+
+  console.log({ event });
+  console.log(JSON.stringify(event.data))
+
+  res.send({ recieved: true });
+}
